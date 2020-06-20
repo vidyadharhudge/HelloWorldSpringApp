4bb1fa9f1b21f017e249f2a3622888014f4aee16
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-06-20 02:26:25 -0700

    Added:-Welcome Message

---
+++ src/main/java/com/example/firstspringapp/FirstspringappApplication.java
@@ -0,0 +1,11 @@
+package com.example.firstspringapp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspringappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstspringappApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/firstspringapp/FirstspringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspringappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
